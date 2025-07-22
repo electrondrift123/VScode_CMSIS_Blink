48db14b0ea95d117b630617e5d7fa8b4a52ae015
Ivan Tuanadatu <ivantuanadatu204@gmail.com>
2025-07-22 20:38:19 +0800

    2nd Commit

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
 #include "stm32f4xx.h"
+
+
+int main(void){
+
+    // Initialize the system clock
+    SystemInit();
+
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; // Enable GPIOA clock
+    GPIOC->MODER  &= ~(3 << (13 * 2)); // Clear mode for PC13
+    GPIOC->MODER  |= (1 << (13 * 2)); // Set
+
+    while (1) {
+        GPIOC->ODR ^= (1 << 13); // Toggle PC13
+        for (volatile int i = 0; i < 100000; i++); // Simple delay
+    }
+}
